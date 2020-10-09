915d2b8b30f37f6722c6a893e076e21d8bca252c
kprotty <45520026+kprotty@users.noreply.github.com>
2020-10-09 11:17:05 -0500

    benchmark with spin + os locks

---
+++ benches/mutex/spin_lock.rs
@@ -0,0 +1,48 @@
+// Copyright (c) 2020 kprotty
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// 	http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+use std::sync::atomic::{AtomicBool, spin_loop_hint, Ordering};
+
+pub struct Lock(AtomicBool);
+
+unsafe impl super::Lock for Lock {
+    const NAME: &'static str = "spin_lock";
+
+    fn new() -> Self {
+        Self(AtomicBool::new(false))
+    }
+
+    fn with(&self, f: impl FnOnce()) {
+        self.acquire();
+        f();
+        self.release();
+    }
+}
+
+impl Lock {
+    fn acquire(&self) {
+        let mut locked = false;
+        loop {
+            if !locked && !self.0.swap(true, Ordering::Acquire) {
+                return;
+            }
+            spin_loop_hint();
+            locked = self.0.load(Ordering::Relaxed);
+        }
+    }
+
+    fn release(&self) {
+        self.0.store(false, Ordering::Release);
+    }
+}
